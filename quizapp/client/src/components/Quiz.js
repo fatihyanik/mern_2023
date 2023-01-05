521b5605b3d6e49c14e5211be6b95d5cbd8de70e
fatihyanik <fatihyanik07@gmail.com>
2023-01-05 18:23:03 +0100

    added mock data and questions components

--- quizapp/client/src/components/Quiz.js
+++ quizapp/client/src/components/Quiz.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import Questions from './Questions';
 
 const Quiz = () => {
-  return (
-    <div>Quiz</div>
-  )
+
+    /**next button event handler */
+    function onNext(){
+        console.log('On next click');
+    }
+
+     /**prev button event handler */
+    function onPrev(){
+        console.log('On prev click');
+    }
+
+
+    return (
+        <div className='container'>
+            <h1 className='title text-light'>Quiz Application</h1>
+
+            {/* display questions*/}
+            <Questions />
+
+            <div className='grid'>
+                <button className='btn prev' onClick={onPrev}>Previous</button>
+                <button className='btn next' onClick={onNext}>Next</button>
+            </div>
+        </div>
+    )
 }
 
 export default Quiz
