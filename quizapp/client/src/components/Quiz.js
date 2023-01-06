45bf3ee1bc0ef682afa57b44c695d3041c6ae1b0
fatihyanik <fatihyanik07@gmail.com>
2023-01-06 20:02:20 +0100

    added move next question

--- quizapp/client/src/components/Quiz.js
+++ quizapp/client/src/components/Quiz.js
@@ -1,37 +1,50 @@
 import React, { useEffect } from 'react'
-import Questions from './Questions';
+import Questions from './Questions'
+
+import { MoveNextQuestion, MovePrevQuestion } from '../hooks/FetchQuestion';
 
 /** redux store import */
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
-const Quiz = () => {
+function Quiz() {
 
-    const state = useSelector(state => state)
+    // const trace = useSelector(state => state.questions.trace);
+    const { queue, trace } = useSelector(state => state.questions);
+    const dispatch = useDispatch()
 
     useEffect(() => {
-       // console.log(state);
+        console.log(trace)
     })
 
-    /**next button event handler */
+    /** next button event handler */
     function onNext() {
-        console.log('On next click');
+        console.log('On next click')
+
+        if (trace < queue.length) {
+            /** increase the trace value by one using MoveNextAction */
+            dispatch(MoveNextQuestion());
+        }
+
     }
 
-    /**prev button event handler */
+    /** Prev button event handler */
     function onPrev() {
-        console.log('On prev click');
+        console.log('On onPrev click')
+        if (trace > 0) {
+            /** decrease the trace value by one using MovePrevQuestion */
+            dispatch(MovePrevQuestion());
+        }
     }
-
 
     return (
         <div className='container'>
             <h1 className='title text-light'>Quiz Application</h1>
 
-            {/* display questions*/}
+            {/* display questions */}
             <Questions />
 
             <div className='grid'>
-                <button className='btn prev' onClick={onPrev}>Previous</button>
+                <button className='btn prev' onClick={onPrev}>Prev</button>
                 <button className='btn next' onClick={onNext}>Next</button>
             </div>
         </div>
