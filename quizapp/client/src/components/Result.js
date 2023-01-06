c0048b05114411b9415c579a7b4183aa08ee9c90
fatihyanik <fatihyanik07@gmail.com>
2023-01-06 20:26:29 +0100

    added update selected option

--- quizapp/client/src/components/Result.js
+++ quizapp/client/src/components/Result.js
@@ -3,54 +3,63 @@ import '../styles/Result.css';
 import { Link } from 'react-router-dom';
 
 import ResultTable from './ResultTable';
+import { useDispatch } from 'react-redux';
+
+
+/** import actions  */
+import { resetAllAction } from '../redux/question_reducer';
+import { resetResultAction } from '../redux/result_reducer';
 
 function Result() {
 
-    function onRestart() {
-        console.log('on Restart')
+    const dispatch = useDispatch()
+
+    function onRestart(){
+        dispatch(resetAllAction())
+        dispatch(resetResultAction())
     }
 
-    return (
-        <div className='container'>
-            <h1 className='title text-light'>Quiz Application</h1>
+  return (
+    <div className='container'>
+        <h1 className='title text-light'>Quiz Application</h1>
 
-            <div className='result flex-center'>
-                <div className='flex'>
-                    <span>Username</span>
-                    <span className='bold'>Daily Tuition</span>
-                </div>
-                <div className='flex'>
-                    <span>Total Quiz Points : </span>
-                    <span className='bold'>50</span>
-                </div>
-                <div className='flex'>
-                    <span>Total Questions : </span>
-                    <span className='bold'>05</span>
-                </div>
-                <div className='flex'>
-                    <span>Total Attempts : </span>
-                    <span className='bold'>03</span>
-                </div>
-                <div className='flex'>
-                    <span>Total Earn Points : </span>
-                    <span className='bold'>30</span>
-                </div>
-                <div className='flex'>
-                    <span>Quiz Result</span>
-                    <span className='bold'>Passed</span>
-                </div>
+        <div className='result flex-center'>
+            <div className='flex'>
+                <span>Username</span>
+                <span className='bold'>Daily Tuition</span>
             </div>
-
-            <div className="start">
-                <Link className='btn' to={'/'} onClick={onRestart}>Restart</Link>
+            <div className='flex'>
+                <span>Total Quiz Points : </span>
+                <span className='bold'>50</span>
             </div>
-
-            <div className="container">
-                {/* result table */}
-                <ResultTable></ResultTable>
+            <div className='flex'>
+                <span>Total Questions : </span>
+                <span className='bold'>05</span>
+            </div>
+            <div className='flex'>
+                <span>Total Attempts : </span>
+                <span className='bold'>03</span>
+            </div>
+            <div className='flex'>
+                <span>Total Earn Points : </span>
+                <span className='bold'>30</span>
+            </div>
+            <div className='flex'>
+                <span>Quiz Result</span>
+                <span className='bold'>Passed</span>
             </div>
         </div>
-    )
+
+        <div className="start">
+            <Link className='btn' to={'/'} onClick={onRestart}>Restart</Link>
+        </div>
+
+        <div className="container">
+            {/* result table */}
+            <ResultTable></ResultTable>
+        </div>
+    </div>
+  )
 }
 
-export default Result
+export default Result;
