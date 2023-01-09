e82f3379c424eea9d6c85191c0ea0eba5e3f13ad
fatihyanik <fatihyanik07@gmail.com>
2023-01-09 16:13:35 +0100

    finish quiz app

--- quizapp/client/src/components/Result.js
+++ quizapp/client/src/components/Result.js
@@ -9,68 +9,74 @@ import { attempts_Number, earnPoints_Number, flagResult } from '../helper/helper
 /** import actions  */
 import { resetAllAction } from '../redux/question_reducer';
 import { resetResultAction } from '../redux/result_reducer';
+import { usePublishResult } from '../hooks/setResult';
 
 
 function Result() {
 
     const dispatch = useDispatch()
-    const { questions: { queue, answers }, result: { result, userId } } = useSelector(state => state)
+    const { questions : { queue ,answers}, result : { result, userId}}  = useSelector(state => state)
 
-    useEffect(() => {
-        console.log(flag)
-    })
-
-    const totalPoints = queue.length * 10;
+    const totalPoints = queue.length * 10; 
     const attempts = attempts_Number(result);
     const earnPoints = earnPoints_Number(result, answers, 10)
     const flag = flagResult(totalPoints, earnPoints)
 
-    function onRestart() {
+
+    /** store user result */
+    usePublishResult({ 
+        result, 
+        username : userId,
+        attempts,
+        points: earnPoints,
+        achived : flag ? "Passed" : "Failed" });
+
+    function onRestart(){
         dispatch(resetAllAction())
         dispatch(resetResultAction())
     }
 
-    return (
-        <div className='container'>
-            <h1 className='title text-light'>Quiz Application</h1>
+  return (
+    <div className='container'>
+        <h1 className='title text-light'>Quiz Application</h1>
 
-            <div className='result flex-center'>
-                <div className='flex'>
-                    <span>Username</span>
-                    <span className='bold'>Murat Fatih</span>
-                </div>
-                <div className='flex'>
-                    <span>Total Quiz Points : </span>
-                    <span className='bold'>{totalPoints || 0}</span>
-                </div>
-                <div className='flex'>
-                    <span>Total Questions : </span>
-                    <span className='bold'>{queue.length || 0}</span>
-                </div>
-                <div className='flex'>
-                    <span>Total Attempts : </span>
-                    <span className='bold'>{attempts || 0}</span>
-                </div>
-                <div className='flex'>
-                    <span>Total Earn Points : </span>
-                    <span className='bold'>{earnPoints || 0}</span>
-                </div>
-                <div className='flex'>
-                    <span>Quiz Result</span>
-                    <span style={{ color: `${flag ? "#2aff95" : "#ff2a66"}` }} className='bold'>{flag ? "Passed" : "Failed"}</span>
-                </div>
+        <div className='result flex-center'>
+            <div className='flex'>
+                <span>Username</span>
+                <span className='bold'>{userId || ""}</span>
             </div>
-
-            <div className="start">
-                <Link className='btn' to={'/'} onClick={onRestart}>Restart</Link>
+            <div className='flex'>
+                <span>Total Quiz Points : </span>
+                <span className='bold'>{totalPoints || 0}</span>
             </div>
-
-            <div className="container">
-                {/* result table */}
-                <ResultTable></ResultTable>
+            <div className='flex'>
+                <span>Total Questions : </span>
+                <span className='bold'>{ queue.length || 0}</span>
+            </div>
+            <div className='flex'>
+                <span>Total Attempts : </span>
+                <span className='bold'>{attempts || 0}</span>
+            </div>
+            <div className='flex'>
+                <span>Total Earn Points : </span>
+                <span className='bold'>{earnPoints || 0}</span>
+            </div>
+            <div className='flex'>
+                <span>Quiz Result</span>
+                <span style={{ color : `${flag ? "#2aff95" : "#ff2a66" }` }} className='bold'>{flag ? "Passed" : "Failed"}</span>
             </div>
         </div>
-    )
+
+        <div className="start">
+            <Link className='btn' to={'/'} onClick={onRestart}>Restart</Link>
+        </div>
+
+        <div className="container">
+            {/* result table */}
+            <ResultTable></ResultTable>
+        </div>
+    </div>
+  )
 }
 
 export default Result;
--- quizapp/client/src/components/ResultTable.js
+++ quizapp/client/src/components/ResultTable.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getServerData } from '../helper/helper'
 
-const ResultTable = () => {
+function ResultTable() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    getServerData(`${process.env.REACT_APP_SERVER_HOSTNAME}/api/result`, (res) => {
+      setData(res)
+    })
+  })
+
   return (
     <div>
       <table>
@@ -13,16 +23,22 @@ const ResultTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className='table-body'>
-            <td>Murat Fatih</td>
-            <td>03</td>
-            <td>20</td>
-            <td>Passed</td>
-          </tr>
+          {!data ?? <div>No Data Found </div>}
+          {
+            data.map((v, i) => (
+              <tr className='table-body' key={i}>
+                <td>{v?.username || ''}</td>
+                <td>{v?.attempts || 0}</td>
+                <td>{v?.points || 0}</td>
+                <td>{v?.achived || ""}</td>
+              </tr>
+            ))
+          }
+
         </tbody>
       </table>
     </div>
   )
 }
 
-export default ResultTable
+export default ResultTable;
