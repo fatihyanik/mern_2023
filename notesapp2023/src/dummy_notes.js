44ff1988ceb52f195b6e7f0c89f60ed41668c35a
fatihyanik <fatihyanik07@gmail.com>
2023-01-22 14:11:30 +0100

    added notes page design

---
+++ notesapp2023/src/dummy_notes.js
@@ -0,0 +1,41 @@
+const notes = [
+    {
+        id:1, 
+        title: 'Murat Fatih',
+        details: 'lorem',
+        date: 'Jan 6 2023'
+    },
+    {
+        id:2, 
+        title: 'Murat Fatih',
+        details: 'lorem',
+        date: 'Jan 6 2023'
+    },
+    {
+        id:3, 
+        title: 'Murat Fatih',
+        details: 'lorem',
+        date: 'Jan 6 2023'
+    },
+    {
+        id:4, 
+        title: 'Murat Fatih',
+        details: 'lorem',
+        date: 'Jan 6 2023'
+    },
+    {
+        id:5, 
+        title: 'Murat Fatih',
+        details: 'lorem',
+        date: 'Jan 6 2023'
+    },
+    {
+        id:1, 
+        title: 'Murat Fatih',
+        details: 'lorem',
+        date: 'Jan 6 2023'
+    },
+    
+]
+
+export default notes;
