ce0ae5418f834bff25897bcae9c97c8a38f0d172
fatihyanik <fatihyanik07@gmail.com>
2023-01-22 14:26:43 +0100

    added edit note and createnote pages

--- notesapp2023/src/pages/CreateNote.jsx
+++ notesapp2023/src/pages/CreateNote.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const CreateNote = () => {
   return (
-    <div>CreateNote</div>
-  )
-}
+    <section>
+      <header className='create-note__header'>
+        <Link to='/' className='btn'>
+          <AiOutlineArrowLeft />
+        </Link>
+        <button className='btn lg primary'>Save</button>
+      </header>
+      <form className='create-note__form'>
+        <input type='text' placeholder='Title' autoFocus />
+        <textarea rows='28' placeholder='Note details...'></textarea>
+      </form>
+    </section>
+  );
+};
 
-export default CreateNote
+export default CreateNote;
