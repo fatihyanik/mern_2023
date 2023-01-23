5ce62dae469020eeac9e0be113a6fce8d45cf918
fatihyanik <fatihyanik07@gmail.com>
2023-01-23 20:23:00 +0100

    finish app

--- notesapp2023/src/components/NoteItem.jsx
+++ notesapp2023/src/components/NoteItem.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 
 const NoteItem = ({ note }) => {
   return (
-    <Link to={`/edit-note/${note.id}`} className='note'>
-      <h4>{note.title > 50 ? note.title.substr(0, 50) + '...' : note.title}</h4>
+    <Link to={`/edit-note/${note.id}`} state={{ note }} className='note'>
+      <h5>
+        {note.title.length > 40 ? note.title.substr(0, 60) + "..." : note.title}
+      </h5>
       <p>{note.date}</p>
     </Link>
   );
